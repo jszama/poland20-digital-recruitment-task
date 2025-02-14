2cdedb4dd3e5f8488467703eea2b16306124071c
jszama <jmszamotulski@gmail.com>
2025-02-14 17:16:16 +0000

    summit section refactored

--- src/app/_components/summit/index.tsx
+++ src/app/_components/summit/index.tsx
@@ -10,42 +10,12 @@ export default function Summit() {
 				<p>Dlaczego nie może Cię zabraknąć na naszej jubileuszowej konferencji?</p>
 			</header>
 			<section className="summit-tiles">
-				<Tile
-					icon={puzzle}
-					iconAlt="Ikona kawałka układanki"
-					title="Spotkanie z ambicją i kreatywnością"
-					description="Dołącz do wspólnoty ambitnych polskich studentów z uczelni z całego świata i zbuduj nowe znajomości!"
-				/>
-				<Tile
-					icon={person}
-					iconAlt="Ikona człowieka"
-					title="Panele dyskusyjne z ekspertami"
-					description="Uczestnicz w dyskusjach z światowej klasy ekspertami, naukowcami oraz liderami biznesu, którzy debatują o najistotniejszych problemach naszych czasów i wpływie technologii na ich rozwiązania."
-				/>
-				<Tile
-					icon={workshop}
-					iconAlt="Ikona warsztatu"
-					title="Interaktywne warsztaty"
-					description="Weź udział w warsztatach prowadzonych przez przedstawicieli wiodących funduszy inwestycyjnych i międzynarodowych korporacji."
-				/>
-				<Tile
-					icon={development}
-					iconAlt="Ikona linii wykresu idzie w górę"
-					title="Rozwój kariery"
-					description="Pokaż się ze swojej najlepszej strony i zdobądź potencjalne praktyki albo inne okazję rozwoju swojej kariery w jednej z obecnych na konferencji firm!"
-				/>
-				<Tile
-					icon={graduateCap}
-					iconAlt="Ikonka czapki absolwenta"
-					title="Edukacja za granicą"
-					description="Dowiedz się jak możemy pomóc Ci studiować na najlepszych uczelniach na świecie!"
-				/>
-				<Tile
-					icon={conversation}
-					iconAlt="Ikona rozmowy"
-					title="Networking i rozrywka"
-					description="Nie przegap okazji do spotkań z innymi studentami i prelegentami podczas bankietu oraz uroczystego balu- idealna szansa na wymianę myśli, zawarcie nowych znajomości i niezapomnianą zabawę."
-				/>
+				<Tile icon={puzzle} iconAlt="Ikona kawałka układanki" title="Spotkanie z ambicją i kreatywnością" description="Dołącz do wspólnoty ambitnych polskich studentów z uczelni z całego świata i zbuduj nowe znajomości!" />
+				<Tile icon={person} iconAlt="Ikona człowieka" title="Panele dyskusyjne z ekspertami" description="Uczestnicz w dyskusjach z światowej klasy ekspertami, naukowcami oraz liderami biznesu, którzy debatują o najistotniejszych problemach naszych czasów i wpływie technologii na ich rozwiązania." />
+				<Tile icon={workshop} iconAlt="Ikona warsztatu" title="Interaktywne warsztaty" description="Weź udział w warsztatach prowadzonych przez przedstawicieli wiodących funduszy inwestycyjnych i międzynarodowych korporacji." />
+				<Tile icon={development} iconAlt="Ikona linii wykresu idzie w górę" title="Rozwój kariery" description="Pokaż się ze swojej najlepszej strony i zdobądź potencjalne praktyki albo inne okazję rozwoju swojej kariery w jednej z obecnych na konferencji firm!" />
+				<Tile icon={graduateCap} iconAlt="Ikonka czapki absolwenta" title="Edukacja za granicą" description="Dowiedz się jak możemy pomóc Ci studiować na najlepszych uczelniach na świecie!" />
+				<Tile icon={conversation} iconAlt="Ikona rozmowy" title="Networking i rozrywka" description="Nie przegap okazji do spotkań z innymi studentami i prelegentami podczas bankietu oraz uroczystego balu- idealna szansa na wymianę myśli, zawarcie nowych znajomości i niezapomnianą zabawę." />
 			</section>
 		</section>
 	);
